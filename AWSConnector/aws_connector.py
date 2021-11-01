edc46c7ec18a4908474eed3b1a960c94b1adc734
Kamil Burkiewicz <kamil.burkiewicz@gmail.com>
2021-11-01 21:22:59 +0100

    Error handling fix 1

--- AWSConnector/aws_connector.py
+++ AWSConnector/aws_connector.py
@@ -2,17 +2,30 @@
 from jupyter_server.base.handlers import JupyterHandler
 import json
 import tornado
+import pathlib
+import os
+
+home = os.getenv('HOME')
 
 class ConnectorHandler(JupyterHandler):
     @tornado.web.authenticated
     def get(self):
-        with open('/root/.aws/credentials', 'r') as f:
-            contents = f.readlines()
-            self.write(json.dumps({'data': ''.join(contents)}))
+        try:
+            with open(home + '/.aws/credentials', 'r') as f:
+                contents = f.read()
+        except FileNotFoundError:
+            contents = ''
+
+        self.write(json.dumps({'data': contents}))
 
     @tornado.web.authenticated
     def put(self):
         data = self.get_json_body()['data']
-        with open('/root/.aws/credentials', 'w') as f:
-            f.write(data)
-            self.write(json.dumps({'status': 'OK'}))
+        try:
+            pathlib.Path(home + '/.aws').mkdir(exist_ok=True)
+            with open(home + '/.aws/credentials', 'w') as f:
+                f.write(data)
+        except FileNotFoundError as e:
+            pass
+
+        self.write(json.dumps({'status': 'OK'}))
